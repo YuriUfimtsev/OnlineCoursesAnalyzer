df459541ee1c79639618420de24acf51680eb5ac
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-12-06 20:49:55 +0300

    fix: remove SetDirectory

--- OnlineCoursesAnalyzerTests/XLSXParserTests.cs
+++ OnlineCoursesAnalyzerTests/XLSXParserTests.cs
@@ -1,5 +1,6 @@
-using OnlineCoursesAnalyzer.DataHandling;
+п»їusing OnlineCoursesAnalyzer.DataHandling;
 using System.Data.Common;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -8,25 +9,26 @@ namespace OnlineCoursesAnalyzerTests;
 public class XLSXParserTests
 {
     public static string GetPathToFile(string xlxsFileNameWithoutExtension)
-        => $"Data/{xlxsFileNameWithoutExtension}";
+        => $"../../../Data/{xlxsFileNameWithoutExtension}";
 
-    [OneTimeSetUp]
-    public void RunBeforeAllTests()
-    {
-        Directory.SetCurrentDirectory("../../../");
-    }
+    //[OneTimeSetUp]
+    //public void RunBeforeAllTests()
+    //{
+    //}
 
     [Test]
     public void StandartTest()
     {
+        Console.WriteLine(TestContext.CurrentContext.TestDirectory);
+        Console.WriteLine(TestContext.CurrentContext.WorkDirectory);
         var stream = File.OpenRead(GetPathToFile("1.xlsx"));
-        var requiredColumnNames = new string[] { "Фамилия", "Имя", "Город" };
+        var requiredColumnNames = new string[] { "Р¤Р°РјРёР»РёСЏ", "РРјСЏ", "Р“РѕСЂРѕРґ" };
         var (dataWithRowNumbers, nullRows) = XLSXParser.GetDataWithoutFirstRow(stream, requiredColumnNames, 0);
         var expectedDataWithRowNumbers = new List<string[]>
         {
-            new string[] { "Анонимов", "Аноним", "Санкт-Петербург", "2" },
-            new string[] { "Петров", "Петр", "Москва", "3" },
-            new string[] { "Иванов", "Иван", "Казань", "4" },
+            new string[] { "РђРЅРѕРЅРёРјРѕРІ", "РђРЅРѕРЅРёРј", "РЎР°РЅРєС‚-РџРµС‚РµСЂР±СѓСЂРі", "2" },
+            new string[] { "РџРµС‚СЂРѕРІ", "РџРµС‚СЂ", "РњРѕСЃРєРІР°", "3" },
+            new string[] { "РРІР°РЅРѕРІ", "РРІР°РЅ", "РљР°Р·Р°РЅСЊ", "4" },
         };
         Assert.That(nullRows.Count, Is.EqualTo(0));
         CollectionAssert.AreEquivalent(expectedDataWithRowNumbers, dataWithRowNumbers);
@@ -36,7 +38,7 @@ public class XLSXParserTests
     public void SomeNullRowsInsideTest()
     {
         var stream = File.OpenRead(GetPathToFile("2.xlsx"));
-        var requiredColumnNames = new string[] { "Фамилия", "Имя", "Город" };
+        var requiredColumnNames = new string[] { "Р¤Р°РјРёР»РёСЏ", "РРјСЏ", "Р“РѕСЂРѕРґ" };
         var (dataWithRowNumbers, nullRows) = XLSXParser.GetDataWithoutFirstRow(stream, requiredColumnNames, 12);
         var expectedNullRowNumbers = new List<string> { "2", "4", "5", "6", "8", "11" };
         Assert.That(dataWithRowNumbers.Count, Is.EqualTo(5));
@@ -47,7 +49,7 @@ public class XLSXParserTests
     public void CompletelyNullRowsAtTheEndOfTheTableTest()
     {
         var stream = File.OpenRead(GetPathToFile("3.xlsx"));
-        var requiredColumnNames = new string[] { "Фамилия", "Имя", "Город" };
+        var requiredColumnNames = new string[] { "Р¤Р°РјРёР»РёСЏ", "РРјСЏ", "Р“РѕСЂРѕРґ" };
         var (dataWithRowNumbers, nullRows) = XLSXParser.GetDataWithoutFirstRow(stream, requiredColumnNames, 12);
         Assert.That(nullRows.Count, Is.EqualTo(0));
         Assert.That(dataWithRowNumbers.Count, Is.EqualTo(11));
@@ -57,7 +59,7 @@ public class XLSXParserTests
     public void NotCompletelyNullRowsAtTheEndOfTheTableTest()
     {
         var stream = File.OpenRead(GetPathToFile("4.xlsx"));
-        var requiredColumnNames = new string[] { "Фамилия", "Имя", "Город" };
+        var requiredColumnNames = new string[] { "Р¤Р°РјРёР»РёСЏ", "РРјСЏ", "Р“РѕСЂРѕРґ" };
         var (dataWithRowNumbers, nullRows) = XLSXParser.GetDataWithoutFirstRow(stream, requiredColumnNames, 12);
         var expectedNullRowNumbers = new List<string> { "9", "10", "11", "12" };
         CollectionAssert.AreEquivalent(expectedNullRowNumbers, nullRows);
@@ -68,7 +70,7 @@ public class XLSXParserTests
     public void MoreNullRowsThanAllowedTest()
     {
         var stream = File.OpenRead(GetPathToFile("5.xlsx"));
-        var requiredColumnNames = new string[] { "Фамилия", "Имя", "Город" };
+        var requiredColumnNames = new string[] { "Р¤Р°РјРёР»РёСЏ", "РРјСЏ", "Р“РѕСЂРѕРґ" };
         Assert.Throws<InvalidInputDataException>(() => XLSXParser.GetDataWithoutFirstRow(
             stream,
             requiredColumnNames,
@@ -79,7 +81,7 @@ public class XLSXParserTests
     public void NotXLSXFileTest()
     {
         var stream = File.OpenRead(GetPathToFile("Empty.pdf"));
-        var requiredColumnNames = new string[] { "Фамилия", "Имя", "Город" };
+        var requiredColumnNames = new string[] { "Р¤Р°РјРёР»РёСЏ", "РРјСЏ", "Р“РѕСЂРѕРґ" };
         Assert.Throws<InvalidInputDataException>(() => XLSXParser.GetDataWithoutFirstRow(
             stream,
             requiredColumnNames,
@@ -90,7 +92,7 @@ public class XLSXParserTests
     public void AllowedNumberOfErrorRowsParameterLessThanZeroTest()
     {
         var stream = File.OpenRead(GetPathToFile("1.xlsx"));
-        var requiredColumnNames = new string[] { "Фамилия", "Имя", "Город" };
+        var requiredColumnNames = new string[] { "Р¤Р°РјРёР»РёСЏ", "РРјСЏ", "Р“РѕСЂРѕРґ" };
         var (dataWithRowNumbers, nullRows) = XLSXParser.GetDataWithoutFirstRow(stream, requiredColumnNames, -5);
     }
 
@@ -109,7 +111,7 @@ public class XLSXParserTests
     public void RequiredColumnNameIsMissingInTheTableTest()
     {
         var stream = File.OpenRead(GetPathToFile("1.xlsx"));
-        var requiredColumnNames = new string[] { "Фамилия", "Имя", "Город", "Регион" };
+        var requiredColumnNames = new string[] { "Р¤Р°РјРёР»РёСЏ", "РРјСЏ", "Р“РѕСЂРѕРґ", "Р РµРіРёРѕРЅ" };
         Assert.Throws<InvalidInputDataException>(() => XLSXParser.GetDataWithoutFirstRow(
             stream,
             requiredColumnNames,
